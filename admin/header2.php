87ddef73b6ece81180131178de1ca3df2f7e33b7
Amir Hasan Zaizakarani <43024238+aihara44@users.noreply.github.com>
2024-11-12 23:39:44 +0800

    Add files via upload

---
+++ admin/header2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Persada Dunya Distribution Center - Operation Homepage</title>
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/custom.css">
+      <link rel="stylesheet" href="../DataTables/css/jquery.dataTables.min.css">
+    <!-- jQuery -->
+      <script src="../js/jquery-1.12.1.min.js"></script>
+      <script src="../js/bootstrap.min.js"></script>
+      
+  </head>
+  <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                <h3><b>Administrator</b></h3>
+            </div>
+        </div>
+        <div class="row">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                     <div class="nav navbar-nav">
+                     <li class="active"><a href="index.php">Home</a></li>
+                    </div>
+                    <div class="nav navbar-nav pull-right">
+                    <li><a href="#"><?php echo "Hello: <b>".$_SESSION["admin_name"]."</b>"; ?></a></li>
+                    <li><a href="logout.php">Log Out</a></li>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </nav>
+        </div>
