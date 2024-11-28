6008f820e1e29666f56ffb4a70f7e85d9b6fd234
Amir Hasan Zaizakarani <amirhasan.zaizakarani@gmail.com>
2024-11-28 16:40:56 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,22 @@
 <?php
-$con = mysqli_connect("fypbns.mysql.database.azure.com", "fyp_admin", "Amirhasan@990630", "persada") or die ("Cannot connect to database");
-function mres ($con,$data){
-    return mysqli_real_escape_string($con,rtrim(ltrim($data)));
+// Fetch environment variables
+$db_host = getenv('fypbns.mysql.database.azure.com');
+$db_username = getenv('fyp_admin');
+$db_password = getenv('Amirhasan@990630');
+$db_name = getenv('persada');
+
+// Establish the connection
+$connection = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+// Check for connection errors
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+} else {
+    echo "Connected successfully to MySQL database!";
 }
+
+// Your database queries go here
+
+// Close the connection when done
+$connection->close();
 ?>
