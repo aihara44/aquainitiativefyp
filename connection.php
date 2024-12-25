77008fd9b11df201c834ddcc1f91d543e26638b8
Amir Hasan Zaizakarani <43024238+aihara44@users.noreply.github.com>
2024-12-25 21:08:09 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,22 +1,19 @@
 <?php
-// Fetch environment variables
-$db_host = getenv('DB_HOST');
-$db_username = getenv('DB_USER');
-$db_password = getenv('DB_PASSWORD');
-$db_name = getenv('DB_NAME');
+$host = getenv('AZURE_MYSQL_HOST');
+$username = getenv('AZURE_MYSQL_USER');
+$password = getenv('AZURE_MYSQL_PASSWORD');
+$database = getenv('Azure_MYSQL_DBNAME');
+$port = int(getenv('AZURE_MYSQL_PORT'));
+# $flag = getenv('AZURE_MYSQL_FLAG');
 
-// Establish the connection
-$connection = new mysqli($db_host, $db_username, $db_password, $db_name);
+$conn = mysqli_init();
+# mysqli_ssl_set($conn,NULL,NULL,NULL,NULL,NULL);
+mysqli_real_connect($conn, $host, $username, $password, $database, $port, NULL);
 
-// Check for connection errors
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-} else {
-    echo "Connected successfully to MySQL database!";
+if (mysqli_connect_errno($conn)) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-// Your database queries go here
-
-// Close the connection when done
-$connection->close();
+echo 'Connected successfully to MySQL database!';
+mysqli_close($conn);
 ?>
